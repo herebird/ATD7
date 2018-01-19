0a93543028a7a73f1a8cde2d2856109494fefbb8
herebird <35319775+herebird@users.noreply.github.com>
2018-01-19 11:24:36 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -414,10 +414,11 @@ service ssh restart
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 110"/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 80"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
-service ssh restart
-service dropbear restart
+echo "/usr/sbin/nologin" >> /etc/shells
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
 
 # Update to Dropbear 2016
 cd
